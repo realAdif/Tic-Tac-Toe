a60c377c0c4124c5fac7ed53d82de13a5c9daad0
Aditya <90451018+realAdif@users.noreply.github.com>
2024-07-17 17:46:00 +1000

    Update homepage layout and add links for new game options

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import HomePage from "./components/HomePage"
 
 export function App() {
   return (
-    <main className="">
+    <main className=" w-[360px] md:w-[460px]">
       <Router>
         <Route path="/" element={() => <HomePage />} />
+        <Route path="/cpu" element={() => <h1>CPU</h1>} />
+        <Route path="/player" element={() => <h1>Player</h1>} />
       </Router>
     </main>
   )
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,18 @@
+import { Link } from "kaioken"
+
 export default function HomePage() {
   return (
-    <div className="flex flex-col gap-10  w-[460px]">
+    <div className="flex flex-col gap-10 w-full">
       {/* top icon */}
       <div className="flex justify-center gap-2">
         <img src="icon-x.svg" alt="icon-x" className="w-8 h-8" />
         <img src="icon-o.svg" alt="icon-o" className="w-8 h-8" />
       </div>
       {/* pick a player */}
-      <div className=" bg-primary rounded-2xl p-3 w-full shadow-[inset_0_-8px_0px_#10212A]">
-        <h1 className=" uppercase text-center py-2">Pick player its</h1>
+      <div className=" bg-primary rounded-2xl p-3 w-full shadow-[inset_0_-9px_0px_#10212A]">
+        <h1 className=" uppercase text-center py-2 tracking-[1px]">
+          Pick player 1's Mark
+        </h1>
         <div className="bg-[#1A2A33] flex p-2 rounded-xl">
           <button className="w-1/2 h-14 rounded-xl mx-auto hover:bg-[#A8BFC9] opacity-50">
             <img
@@ -25,16 +29,22 @@ export default function HomePage() {
             />
           </button>
         </div>
-        <p className=" uppercase text-center py-2">Remember: X Goes first</p>
+        <p className="uppercase text-center py-2">Remember: X Goes first</p>
       </div>
       {/* button */}
       <div className="flex flex-col gap-5">
-        <button className="w-full bg-light-yellow text-[#1A2A33] h-[67px] rounded-xl shadow-[inset_0_-8px_0px_#CC8B13]">
-          <h2>NEW GAME (VS CPU)</h2>
-        </button>
-        <button className="w-full bg-light-blue text-[#1A2A33] h-[67px] rounded-xl shadow-[inset_0_-8px_0px_#118C87]">
-          <h2>NEW GAME (VS PLAYER)</h2>
-        </button>
+        <Link
+          to="/cpu"
+          className="w-full  bg-light-yellow hover:bg-[#FFC860] text-[#1A2A33] h-[67px] rounded-xl shadow-[inset_0_-9px_0px_#CC8B13]"
+        >
+          <h2 className="mt-2 text-center">NEW GAME (VS CPU)</h2>
+        </Link>
+        <Link
+          to="/player"
+          className="w-full bg-light-blue hover:bg-[#65E9E4] text-[#1A2A33] h-[67px] rounded-xl shadow-[inset_0_-9px_0px_#118C87]"
+        >
+          <h2 className="mt-2 text-center">NEW GAME (VS PLAYER)</h2>
+        </Link>
       </div>
     </div>
   )
